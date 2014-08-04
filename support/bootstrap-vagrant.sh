997110e93f2f226c39b5d39482bdedd7e300a92d
David Cramer <cramer@dropbox.com>
2014-08-04 13:44:11 -0700

    Add fpm to vm

--- support/bootstrap-vagrant.sh
+++ support/bootstrap-vagrant.sh
@@ -3,4 +3,10 @@
 export DEBIAN_FRONTEND=noninteractive
 
 sudo apt-get update -y
+
+# Install lxc
 sudo apt-get install -y libcgmanager0 lxc
+
+# Install fpm
+sudo apt-get install -y rubygems
+sudo gem install fpm --no-ri --no-rdoc
