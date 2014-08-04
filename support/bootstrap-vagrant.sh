796ac1e7918947f146b01b5e0071890057f12119
David Cramer <cramer@dropbox.com>
2014-08-04 13:46:45 -0700

    Correct dependencies for trusty

--- support/bootstrap-vagrant.sh
+++ support/bootstrap-vagrant.sh
@@ -8,5 +8,5 @@ sudo apt-get update -y
 sudo apt-get install -y libcgmanager0 lxc
 
 # Install fpm
-sudo apt-get install -y rubygems
+sudo apt-get install -y ruby-dev gcc
 sudo gem install fpm --no-ri --no-rdoc
