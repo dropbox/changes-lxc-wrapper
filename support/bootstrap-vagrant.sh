6b9a8d18898e6d33c21e1968ee2f02ddc2750142
David Cramer <cramer@dropbox.com>
2014-09-15 16:00:23 -0700

    Split up vagrant/ubuntu bootstrap

--- support/bootstrap-vagrant.sh
+++ support/bootstrap-vagrant.sh
@@ -1,27 +1,7 @@
 #!/bin/bash -eux
 
-export DEBIAN_FRONTEND=noninteractive
-
-sudo apt-get install -y python-software-properties software-properties-common
-sudo add-apt-repository -y ppa:awstools-dev/awstools
-
-sudo apt-get update -y
-
-# Install basic Python support
-sudo apt-get install -y python3 python3-setuptools python3-pip python-virtualenv
-
-# Install aws cli tools
-sudo apt-get install -y awscli
-
-# Install git
-sudo apt-get install -y git
-
-# Install lxc
-sudo apt-get install -y libcgmanager0 lxc
-
-# Install fpm
-sudo apt-get install -y ruby-dev gcc
-sudo gem install fpm --no-ri --no-rdoc
-
 cd /vagrant/
+
+support/bootstrap-ubuntu.sh
+
 sudo pip3 install -e .
